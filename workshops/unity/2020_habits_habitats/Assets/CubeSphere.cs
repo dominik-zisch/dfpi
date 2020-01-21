f06e7881d67d195c35579c56198f157128aa46ba
George Adamopoulos <30707361+GeorgeAdamon@users.noreply.github.com>
2020-01-21 02:10:15 +0000

    fixed gitignore

---
+++ workshops/unity/2020_habits_habitats/Assets/CubeSphere.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSphere : MonoBehaviour
+{
+    public int gridSize;
+    [Range(0,10)]
+    public float radius = 1;
+    
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Vector3[] normals;
+    private Color32[] cubeUV;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void Generate () 
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Sphere";
+        CreateVertices();
+        CreateTriangles();
+        CreateColliders();
+    }
+    
+    private void SetVertex (int i, int x, int y, int z) {
+        Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
+        normals[i] = v.normalized;
+        vertices[i] = normals[i] * radius;
+        cubeUV[i] = new Color32((byte)x, (byte)y, (byte)z, 0);
+    }
+
+    private void CreateColliders()
+    {
+        gameObject.AddComponent<SphereCollider>();
+    }
+
+    private void CreateTriangles()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void CreateVertices()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ workshops/unity/2020_habits_habitats/Assets/SphericalDistance.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SphericalDistance : MonoBehaviour
+{
+    public Transform A;
+
+    public Transform B;
+
+    public float Radius;
+
+    public float GreatCircleDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.one * Radius * 2;
+                               
+        var v1 = A.position - transform.position;
+        var v2 = B.position - transform.position;
+
+        var sv1 = v1.normalized * Radius;
+        var sv2 = v2.normalized * Radius;
+
+        
+        
+        GreatCircleDistance = Mathf.Acos(Vector3.Dot(sv1, sv2));
+        
+        Debug.DrawLine(transform.position, v1);
+        Debug.DrawLine(transform.position, v2);
+        
+        Debug.DrawLine(transform.position, sv1, Color.red);
+        Debug.DrawLine(transform.position, sv2, Color.red);
+        
+        
+        GetComponent<MeshRenderer>().sharedMaterial.SetVector("_PosA", sv1);
+        GetComponent<MeshRenderer>().sharedMaterial.SetVector("_PosB", sv2);
+    }
+}
