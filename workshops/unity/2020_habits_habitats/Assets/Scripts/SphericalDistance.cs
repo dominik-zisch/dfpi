cd2ad656513e9207d2d4e3c7d0e1b251fdd85e16
George Adamopoulos <30707361+GeorgeAdamon@users.noreply.github.com>
2020-01-23 19:45:25 +0000

    Cleaned up folders

--- workshops/unity/2020_habits_habitats/Assets/Scripts/SphericalDistance.cs
+++ workshops/unity/2020_habits_habitats/Assets/Scripts/SphericalDistance.cs
@@ -12,14 +12,14 @@ public class SphericalDistance : MonoBehaviour
     public float Radius;
 
     public float GreatCircleDistance;
+
+    private MeshRenderer mr;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        mr = GetComponent<MeshRenderer>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.localScale = Vector3.one * Radius * 2;
@@ -29,19 +29,10 @@ public class SphericalDistance : MonoBehaviour
 
         var sv1 = v1.normalized ;
         var sv2 = v2.normalized ;
-
-        
         
         GreatCircleDistance = Mathf.Acos(Vector3.Dot(sv1, sv2));
         
-        Debug.DrawLine(transform.position, v1);
-        Debug.DrawLine(transform.position, v2);
-        
-        Debug.DrawLine(transform.position, sv1, Color.red);
-        Debug.DrawLine(transform.position, sv2, Color.red);
-        
-        
-        GetComponent<MeshRenderer>().sharedMaterial.SetVector("_PosA", sv1);
-        GetComponent<MeshRenderer>().sharedMaterial.SetVector("_PosB", sv2);
+        mr.sharedMaterial.SetVector("_PosA", sv1);
+        mr.sharedMaterial.SetVector("_PosB", sv2);
     }
 }
