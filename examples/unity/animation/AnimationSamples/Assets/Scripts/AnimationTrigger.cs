e5594335bf7ee6d0aacd7706ec55c494b07178c8
GeorgeAdamon <ga.adamon@gmail.com>
2020-09-23 15:10:32 +0100

    Added first animation sample

---
+++ examples/unity/animation/AnimationSamples/Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    public Animator ourHappyAnimator;
+
+    public bool IsInLoop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsInLoop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ourHappyAnimator == null)
+        {
+            Debug.Log("YOU FORGOT THE ANIMATOR YOU STUPID");
+            return;
+        }
+        
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        
+        
+        if (IsInLoop)
+        {
+            ourHappyAnimator.SetTrigger("ExitAnimation");
+            IsInLoop = false;
+        }
+        else
+        {
+            ourHappyAnimator.SetTrigger("EnterAnimation");
+            IsInLoop = true;
+        }
+    }
+}
