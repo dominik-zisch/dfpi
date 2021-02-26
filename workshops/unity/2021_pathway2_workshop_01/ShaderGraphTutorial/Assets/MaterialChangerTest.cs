fd9073e5d26e1136b9c5050f7d12234b31cc3800
GeorgeAdamon <30707361+GeorgeAdamon@users.noreply.github.com>
2021-02-26 00:08:14 +0000

    Added ShaderGraph tutorial

---
+++ workshops/unity/2021_pathway2_workshop_01/ShaderGraphTutorial/Assets/MaterialChangerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangerTest : MonoBehaviour
+{
+        void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Material mat =  GetComponent<MeshRenderer>().material;
+      
+            mat.SetColor("_ColorA", Random.ColorHSV()); 
+            mat.SetColor("_ColorB", Random.ColorHSV());
+
+        }
+    }
+}
